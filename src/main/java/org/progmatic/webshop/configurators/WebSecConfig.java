7cb97315acba384ee31233420f05a762725e7fce
Ria <benedek.maria88@gmail.com>
2021-02-10 15:28:57 +0100

    configurators.WebSecConfig has been written, but with some TODO

--- src/main/java/org/progmatic/webshop/configurators/WebSecConfig.java
+++ src/main/java/org/progmatic/webshop/configurators/WebSecConfig.java
@@ -1,4 +1,41 @@
 package org.progmatic.webshop.configurators;
 
-public class WebSecConfig {
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class WebSecConfig extends WebSecurityConfigurerAdapter {
+
+    @SuppressWarnings("deprecation")
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        /* TODO
+            have to clear endpoints...
+            and have to clear who has access to what...
+         */
+        http.formLogin()
+                .loginPage("/login")
+                .permitAll()
+                .defaultSuccessUrl("/home", true)
+                .and()
+                .logout()
+                .logoutSuccessUrl("/login")
+                .and()
+                .authorizeRequests()
+                .antMatchers("/products", "/products/**", "/", "/home",
+                        "/webjars/**", "/js/**","/css/**", "/register").permitAll()
+                .anyRequest().authenticated();
+    }
+
 }
