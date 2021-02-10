3b7c764ec4e3cb7f8540ca283ef99aaa8d5f4aff
aronjt <jtelekiaron@gmail.com>
2021-02-10 14:59:48 +0100

    MySQL autologin

--- src/main/java/org/progmatic/webshop/model/User.java
+++ src/main/java/org/progmatic/webshop/model/User.java
@@ -1,4 +1,14 @@
 package org.progmatic.webshop.model;
 
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
 public class User {
+
+    @Id
+    private int id;
+    private String username;
+
+
 }
