c21c5db4fed383089c09df6557f099c7057574c6
Ria <benedek.maria88@gmail.com>
2021-02-10 21:31:30 +0100

    helpers package added with some helpers class to help adding non-changing data to the database (like the type of the clothes)

---
+++ src/main/java/org/progmatic/webshop/helpers/ClothDataHelper.java
@@ -0,0 +1,19 @@
+package org.progmatic.webshop.helpers;
+
+public class ClothDataHelper {
+
+    public static final String GENDER_UNISEX = "unisex";
+    public static final String GENDER_MALE = "male";
+    public static final String GENDER_FEMALE = "female";
+    public static final String GENDER_CHILD = "child";
+
+    public static final String TYPE_TSHIRT = "t-shirt";
+    public static final String TYPE_PULLOVER = "pullover";
+    public static final String TYPE_PANTS = "pants";
+
+    public static final String SIZE_S = "S";
+    public static final String SIZE_M = "M";
+    public static final String SIZE_L = "L";
+    public static final String SIZE_XL = "XL";
+
+}
