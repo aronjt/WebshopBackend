12aa4991caaf372da4f4fab46b120a3c3f5196dc
Ria <benedek.maria88@gmail.com>
2021-02-11 10:43:36 +0100

    added @Transactional to autodata.DataLoader's "run" method, because it works only with that annotation lol

--- src/main/java/org/progmatic/webshop/autodata/DataLoader.java
+++ src/main/java/org/progmatic/webshop/autodata/DataLoader.java
@@ -9,6 +9,7 @@ import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
 @Component
 public class DataLoader implements ApplicationRunner {
@@ -26,6 +27,7 @@ public class DataLoader implements ApplicationRunner {
     }
 
     @Override
+    @Transactional
     public void run(ApplicationArguments args) throws Exception {
         createAdmin();
     }
