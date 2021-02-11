912348df6232eb306c0dfd88a3baddec1d8536d5
Ria <benedek.maria88@gmail.com>
2021-02-11 23:13:41 +0100

    MyUserDetailsService created, UserController started

--- src/main/java/org/progmatic/webshop/controllers/UserController.java
+++ src/main/java/org/progmatic/webshop/controllers/UserController.java
@@ -1,7 +1,23 @@
 package org.progmatic.webshop.controllers;
 
+import org.progmatic.webshop.services.MyUserDetailsService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class UserController {
+
+    private static final Logger LOG = LoggerFactory.getLogger(UserController.class);
+
+    private MyUserDetailsService userService;
+
+    @Autowired
+    public UserController(MyUserDetailsService userService) {
+        this.userService = userService;
+    }
+
+
+
 }
--- src/main/java/org/progmatic/webshop/services/MyUserDetailsService.java
+++ src/main/java/org/progmatic/webshop/services/MyUserDetailsService.java
@@ -1,13 +1,54 @@
 package org.progmatic.webshop.services;
 
+import org.progmatic.webshop.model.User;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
+import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.PersistenceContext;
+
+@Service
 public class MyUserDetailsService implements UserDetailsService {
 
-    @Override
-    public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        return null;
+    @PersistenceContext
+    EntityManager em;
+
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public MyUserDetailsService(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
     }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        return em.createQuery("select u from User u where u.email = :email", User.class)
+                .setParameter("email", email)
+                .getSingleResult();
+    }
+
+    public boolean userExists(String email) {
+        try {
+            User user = em.createQuery("select u from User u where u.email = :email", User.class)
+                    .setParameter("email", email)
+                    .getSingleResult();
+            return true;
+        }
+        catch (NoResultException ex){
+            return false;
+        }
+    }
+
+    @Transactional
+    public void createUser(User user) {
+        user.setPassword(passwordEncoder.encode(user.getPassword()));
+        em.persist(user);
+    }
+
 }
