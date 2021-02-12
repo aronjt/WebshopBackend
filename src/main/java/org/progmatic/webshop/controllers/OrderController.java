cfff0d8b731bdc2a4b51d9c35d672b9e52c9ae54
Ria <benedek.maria88@gmail.com>
2021-02-12 16:00:40 +0100

    added @Service to OrderService, also added endpoints as comment to OrderController

--- src/main/java/org/progmatic/webshop/controllers/OrderController.java
+++ src/main/java/org/progmatic/webshop/controllers/OrderController.java
@@ -4,4 +4,19 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class OrderController {
+
+    /* endpoints
+        /order
+            post
+            add new order
+        /orders
+            get
+            list all orders (admin)
+        /orders/{id}
+            get
+            list one order (admin)
+        /orders/{id}
+            put
+            edit one order (admin)
+     */
 }
--- src/main/java/org/progmatic/webshop/services/OrderService.java
+++ src/main/java/org/progmatic/webshop/services/OrderService.java
@@ -1,4 +1,7 @@
 package org.progmatic.webshop.services;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class OrderService {
 }
