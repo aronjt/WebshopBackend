dfd6709806a0e8dbfbfb0d0a1c1f62d179a1ef83
Mateskoda <skodamate93@gmail.com>
2021-02-15 09:41:46 +0100

    ImageController and ImageService added

---
+++ src/main/java/org/progmatic/webshop/services/ImageService.java
@@ -0,0 +1,70 @@
+package org.progmatic.webshop.services;
+
+import org.progmatic.webshop.dto.ClothDto;
+import org.progmatic.webshop.model.Clothes;
+import org.progmatic.webshop.model.Image;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.zip.DataFormatException;
+import java.util.zip.Deflater;
+import java.util.zip.Inflater;
+@Service
+public class ImageService {
+    private static Logger LOG = LoggerFactory.getLogger(ImageService.class);
+
+    @PersistenceContext
+    EntityManager em;
+
+//    @Transactional
+//    public List<Image> getAllImages() {
+//        List<Image> images = em.createQuery("SELECT i FROM Image i", Image.class).getResultList();
+//        return images;
+//    }
+
+    // compress the image bytes before storing it in the database
+    public byte[] compressBytes(byte[] data) {
+        Deflater deflater = new Deflater();
+        deflater.setInput(data);
+        deflater.finish();
+
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        while (!deflater.finished()) {
+            int count = deflater.deflate(buffer);
+            outputStream.write(buffer, 0, count);
+        }
+        try {
+            outputStream.close();
+        } catch (IOException e) {
+        }
+        System.out.println("Compressed Image Byte Size - " + outputStream.toByteArray().length);
+
+        return outputStream.toByteArray();
+    }
+
+    // uncompress the image bytes before returning it to the angular application
+    public  byte[] decompressBytes(byte[] data) {
+        Inflater inflater = new Inflater();
+        inflater.setInput(data);
+        ByteArrayOutputStream outputStream = new ByteArrayOutputStream(data.length);
+        byte[] buffer = new byte[1024];
+        try {
+            while (!inflater.finished()) {
+                int count = inflater.inflate(buffer);
+                outputStream.write(buffer, 0, count);
+            }
+            outputStream.close();
+        } catch (IOException | DataFormatException i) {
+        }
+        return outputStream.toByteArray();
+    }
+}
