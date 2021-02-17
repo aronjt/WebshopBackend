039d370f9d3cd863b9a95053b69330b262cbd745
Ria <benedek.maria88@gmail.com>
2021-02-17 10:03:04 +0100

    added getCsrfToken method to HomeController

--- src/main/java/org/progmatic/webshop/controllers/HomeController.java
+++ src/main/java/org/progmatic/webshop/controllers/HomeController.java
@@ -1,7 +1,15 @@
 package org.progmatic.webshop.controllers;
 
+import org.springframework.security.web.csrf.CsrfToken;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @GetMapping("/csrf")
+    public CsrfToken getCsrf(CsrfToken token) {
+        return token;
+    }
+
 }
