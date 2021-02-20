c0b8c88e9d1f34b0c9fcdf696a405de4197b862c
Ria <benedek.maria88@gmail.com>
2021-02-20 14:27:26 +0100

    added new test: SearchControllerTest; also removed unused imports from UserControllerTest

---
+++ src/test/java/org/progmatic/webshop/SearchControllerTest.java
@@ -0,0 +1,40 @@
+package org.progmatic.webshop;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
+@ActiveProfiles("test")
+@AutoConfigureMockMvc
+class SearchControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void get_all_genders() throws Exception {
+        mockMvc.perform(
+                get("/genders"))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+    @Test
+    void get_all_types() throws Exception {
+        mockMvc.perform(
+                get("/types"))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
+
+}
