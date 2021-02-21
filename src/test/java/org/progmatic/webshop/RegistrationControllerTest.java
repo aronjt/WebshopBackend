a8c33f255a02ab07810ed4470e6f6259d1574361
Ria <benedek.maria88@gmail.com>
2021-02-21 21:08:42 +0100

    deleted unused import

--- src/test/java/org/progmatic/webshop/RegistrationControllerTest.java
+++ src/test/java/org/progmatic/webshop/RegistrationControllerTest.java
@@ -22,7 +22,6 @@ import javax.persistence.PersistenceContext;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
