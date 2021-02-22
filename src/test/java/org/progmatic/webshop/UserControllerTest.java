c447e3214bff8bea879c936c5fc1ded32d62465f
Ria <benedek.maria88@gmail.com>
2021-02-22 22:53:49 +0100

    deleted unused import

--- src/test/java/org/progmatic/webshop/UserControllerTest.java
+++ src/test/java/org/progmatic/webshop/UserControllerTest.java
@@ -1,6 +1,5 @@
 package org.progmatic.webshop;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
