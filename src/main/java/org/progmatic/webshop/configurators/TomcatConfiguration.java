1f3a2cecf66adb1640abbb15d692c557543ca5db
aronjt <jtelekiaron@gmail.com>
2021-02-23 19:47:53 +0100

    TomcatConfiguration changed to lambda

--- src/main/java/org/progmatic/webshop/configurators/TomcatConfiguration.java
+++ src/main/java/org/progmatic/webshop/configurators/TomcatConfiguration.java
@@ -1,10 +1,8 @@
 package org.progmatic.webshop.configurators;
 
-import org.apache.catalina.Context;
 import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
 import org.springframework.context.annotation.Bean;
