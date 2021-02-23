d3bd42cedd51a786c142f217a6475184dd9b0329
Ria <benedek.maria88@gmail.com>
2021-02-23 09:39:37 +0100

    refactored OrderControllerTest

--- src/test/java/org/progmatic/webshop/OrderControllerTest.java
+++ src/test/java/org/progmatic/webshop/OrderControllerTest.java
@@ -1,13 +1,10 @@
 package org.progmatic.webshop;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.progmatic.webshop.dto.OrderDto;
 import org.progmatic.webshop.helpers.EmailSenderHelper;
-import org.progmatic.webshop.model.OnlineOrder;
-import org.progmatic.webshop.returnmodel.Feedback;
 import org.progmatic.webshop.returnmodel.Message;
+import org.progmatic.webshop.testservice.TestService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -19,13 +16,7 @@ import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.ArrayList;
-import java.util.List;
-
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -39,36 +30,35 @@ class OrderControllerTest {
     private MockMvc mockMvc;
 
     @Autowired
-    private ObjectMapper objectMapper;
-
-    @PersistenceContext
-    EntityManager em;
-
-    final String USER_EMAIL = "ertekelek@ertek.el";
+    TestService service;
 
     @Test
     void send_order_without_authentication() throws Exception {
-        String json = objectMapper.writeValueAsString(createOrder());
-        mockMvc.perform(
-                post("/orders")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().is(302))
-                .andReturn();
+        String json = service.createJson(service.createOrder());
+        if (json != null) {
+            mockMvc.perform(
+                    post("/orders")
+                            .with(SecurityMockMvcRequestPostProcessors.csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(json))
+                    .andExpect(status().is(302))
+                    .andReturn();
+        }
     }
 
     @Test
-    @WithUserDetails(USER_EMAIL)
+    @WithUserDetails(TestService.USER_EMAIL)
     void send_order_with_authentication() throws Exception {
-        String json = objectMapper.writeValueAsString(createOrder());
-        mockMvc.perform(
-                post("/orders")
-                        .with(SecurityMockMvcRequestPostProcessors.csrf())
-                        .contentType(MediaType.APPLICATION_JSON)
-                        .content(json))
-                .andExpect(status().isOk())
-                .andReturn();
+        String json = service.createJson(service.createOrder());
+        if (json != null) {
+            mockMvc.perform(
+                    post("/orders")
+                            .with(SecurityMockMvcRequestPostProcessors.csrf())
+                            .contentType(MediaType.APPLICATION_JSON)
+                            .content(json))
+                    .andExpect(status().isOk())
+                    .andReturn();
+        }
     }
 
     @Test
@@ -81,7 +71,7 @@ class OrderControllerTest {
     }
 
     @Test
-    @WithUserDetails(USER_EMAIL)
+    @WithUserDetails(TestService.USER_EMAIL)
     void get_all_orders_as_user() throws Exception {
         mockMvc.perform(
                 get("/orders"))
@@ -96,14 +86,18 @@ class OrderControllerTest {
                 get("/orders/{id}", 1))
                 .andExpect(status().isOk())
                 .andReturn();
+
         String response = result.getResponse().getContentAsString();
-        Message fb = objectMapper.readValue(response, Message.class);
-        assertFalse(fb.isSuccess());
-        assertEquals("order cannot be found", fb.getMessage());
+        Message fb = service.createObject(response, Message.class);
+
+        if (fb != null) {
+            assertFalse(fb.isSuccess());
+            assertEquals("order cannot be found", fb.getMessage());
+        }
     }
 
     @Test
-    @WithUserDetails(USER_EMAIL)
+    @WithUserDetails(TestService.USER_EMAIL)
     void get_not_existing_order_as_user() throws Exception {
         mockMvc.perform(
                 get("/orders/{id}", 1))
@@ -114,51 +108,40 @@ class OrderControllerTest {
     @Test
     @WithUserDetails(EmailSenderHelper.ADMIN_EMAIL_ADDRESS)
     void get_existing_order_as_admin() throws Exception {
-        long id = getOrderId();
-        MvcResult result = mockMvc.perform(
-                get("/orders/{id}", id))
-                .andExpect(status().isOk())
-                .andReturn();
-        String response = result.getResponse().getContentAsString();
-        assertTrue(response.contains("totalPrice"));
+        long id = service.getOneOrderId();
+
+        if (id != -1) {
+            MvcResult result = mockMvc.perform(
+                    get("/orders/{id}", id))
+                    .andExpect(status().isOk())
+                    .andReturn();
+            String response = result.getResponse().getContentAsString();
+            assertTrue(response.contains("totalPrice"));
+        }
     }
 
     @Test
     @WithUserDetails(EmailSenderHelper.ADMIN_EMAIL_ADDRESS)
     void finish_order_as_admin() throws Exception {
-        long id = getOrderId();
+        long id = service.getOneOrderId();
 
-        mockMvc.perform(
-                put("/orders/{id}", id)
-                        .with(SecurityMockMvcRequestPostProcessors.csrf()))
-                .andExpect(status().isOk())
-                .andReturn();
+        if (id != -1) {
+            mockMvc.perform(
+                    put("/orders/{id}", id)
+                            .with(SecurityMockMvcRequestPostProcessors.csrf()))
+                    .andExpect(status().isOk())
+                    .andReturn();
+        }
     }
 
     @Test
-    @WithUserDetails(USER_EMAIL)
+    @WithUserDetails(TestService.USER_EMAIL)
     void finish_order_as_user() throws Exception {
         mockMvc.perform(
                 put("/orders/{id}", 1)
                         .with(SecurityMockMvcRequestPostProcessors.csrf()))
                 .andExpect(status().is(403))
                 .andReturn();
-    }
-
-    private long getOrderId() {
-        List<OnlineOrder> orders = em.createQuery("SELECT o FROM OnlineOrder o", OnlineOrder.class)
-                .getResultList();
-        if (orders.size() > 0) {
-            return orders.get(0).getId();
-        }
-        return 0;
-    }
-
-    private OrderDto createOrder() {
-        OrderDto order = new OrderDto();
-        order.setTotalPrice(39.99f);
-        order.setPurchasedClothesList(new ArrayList<>());
-        return order;
     }
 
 }
--- src/test/java/org/progmatic/webshop/testservice/TestService.java
+++ src/test/java/org/progmatic/webshop/testservice/TestService.java
@@ -2,8 +2,10 @@ package org.progmatic.webshop.testservice;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.apache.commons.io.IOUtils;
+import org.progmatic.webshop.dto.OrderDto;
 import org.progmatic.webshop.helpers.ImageHelper;
 import org.progmatic.webshop.model.Image;
+import org.progmatic.webshop.model.OnlineOrder;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.mock.web.MockMultipartFile;
 import org.springframework.stereotype.Service;
@@ -12,6 +14,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import java.io.File;
 import java.io.FileInputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -22,6 +25,24 @@ public class TestService {
 
     @PersistenceContext
     EntityManager em;
+
+    public static final String USER_EMAIL = "ertekelek@ertek.el";
+
+    public String createJson(Object o) {
+        try {
+            return objectMapper.writeValueAsString(o);
+        } catch (Exception e) {
+            return null;
+        }
+    }
+
+    public <T> T createObject(String json, Class<T> toReturn) {
+        try {
+            return objectMapper.readValue(json, toReturn);
+        } catch (Exception e) {
+            return null;
+        }
+    }
 
     public long getOneImageId() {
         List<Image> images = em.createQuery("SELECT i FROM Image i", Image.class).getResultList();
@@ -40,6 +61,22 @@ public class TestService {
         } catch (Exception e) {
             return null;
         }
+    }
+
+    public long getOneOrderId() {
+        List<OnlineOrder> orders = em.createQuery("SELECT o FROM OnlineOrder o", OnlineOrder.class)
+                .getResultList();
+        if (orders.size() > 0) {
+            return orders.get(0).getId();
+        }
+        return -1;
+    }
+
+    public OrderDto createOrder() {
+        OrderDto order = new OrderDto();
+        order.setTotalPrice(39.99f);
+        order.setPurchasedClothesList(new ArrayList<>());
+        return order;
     }
 
 }
