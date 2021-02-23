5703d5ccf97da197aff4aed4bf5b73bc5de945b8
aronjt <jtelekiaron@gmail.com>
2021-02-23 19:47:28 +0100

    TomcatConfiguration changed to lambda

--- src/main/java/org/progmatic/webshop/configurators/TomcatConfiguration.java
+++ src/main/java/org/progmatic/webshop/configurators/TomcatConfiguration.java
@@ -2,6 +2,8 @@ package org.progmatic.webshop.configurators;
 
 import org.apache.catalina.Context;
 import org.apache.tomcat.util.http.Rfc6265CookieProcessor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.web.embedded.tomcat.TomcatContextCustomizer;
 import org.springframework.boot.web.embedded.tomcat.TomcatServletWebServerFactory;
 import org.springframework.boot.web.server.WebServerFactoryCustomizer;
@@ -11,22 +13,15 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class TomcatConfiguration {
 
+    public static final Logger LOGGER = LoggerFactory.getLogger(TomcatConfiguration.class);
+
     @Bean
     WebServerFactoryCustomizer<TomcatServletWebServerFactory> cookieProcessorCustomizer() {
-        return new WebServerFactoryCustomizer<TomcatServletWebServerFactory>() {
-
-            @Override
-            public void customize(TomcatServletWebServerFactory tomcatServletWebServerFactory) {
-                tomcatServletWebServerFactory.addContextCustomizers(new TomcatContextCustomizer() {
-
-                    @Override
-                    public void customize(Context context) {
-                        Rfc6265CookieProcessor processor=new Rfc6265CookieProcessor();
-                        processor.setSameSiteCookies("None");
-                        context.setCookieProcessor(processor);
-                    }
-                });
-            }
-        };
+        LOGGER.info("TomcatConfiguration cookieProcessorCustomizer called");
+        return tomcatServletWebServerFactory -> tomcatServletWebServerFactory.addContextCustomizers(context -> {
+            Rfc6265CookieProcessor processor = new Rfc6265CookieProcessor();
+            processor.setSameSiteCookies("None");
+            context.setCookieProcessor(processor);
+        });
     }
 }
