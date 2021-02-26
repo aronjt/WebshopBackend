432ccc784318cf20e06aa1f78c86a454d58feba2
Ria <benedek.maria88@gmail.com>
2021-02-26 15:25:41 +0100

    clothesId is now id

--- src/main/java/org/progmatic/webshop/dto/PurchasedClothDto.java
+++ src/main/java/org/progmatic/webshop/dto/PurchasedClothDto.java
@@ -4,7 +4,7 @@ import org.progmatic.webshop.model.PurchasedClothes;
 
 public class PurchasedClothDto {
 
-    private long clothesId;
+    private long id;
     private String name;
     private int quantity;
     private String size;
@@ -14,14 +14,14 @@ public class PurchasedClothDto {
     public PurchasedClothDto() {}
 
     public PurchasedClothDto(PurchasedClothes cloth) {
-        clothesId = cloth.getClothes().getId();
+        id = cloth.getClothes().getId();
         quantity = cloth.getQuantity();
         size = cloth.getSize();
         name = cloth.getClothes().getName();
     }
 
-    public long getClothesId() {
-        return clothesId;
+    public long getId() {
+        return id;
     }
 
     public int getQuantity() {
